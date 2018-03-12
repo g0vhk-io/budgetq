f24cb8f59ced8d87022203bd94647973984337a1
Tony Yip <tony@opensource.hk>
2018-03-12 20:52:22 +0800

    Use function to create store

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import reducers from './reducers';
+import createStore from './store';
 
-const store = createStore(
-  reducers,
-  applyMiddleware(thunk),
-);
-
-
+const store = createStore();
 ReactDOM.render(
   <Provider store={store}>
     <App />
